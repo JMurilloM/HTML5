9c1878f2d90effb7a26d302e1b12dae5a92d06c6
manuelalanis <alanism@uabc.edu.mx>
2014-06-29 12:26:25 -0700

    add site

---
+++ Pro HTML5/main.js
@@ -0,0 +1,44 @@
+.learn-more a{
+    color:#00b0ff;
+    }
+
+    .learn-more h3{
+        font-family:'Shift',sans-serif;
+        font-size:18px;
+        font-weight:bold;
+        }
+
+        .learn-more{
+            background-color:#f7f7f7;
+            }
+
+            .jumbotron p{
+                color:#fff;
+                font-size:20px;
+                }
+
+                .jumbotron h1{
+                    color:#fff;
+                    font-size:48px;
+                    font-weight:bold;
+                    font-family:'Shift',sans-serif;
+                    }
+
+                    .jumbotron{
+                        height:500px;
+                        background-image:url('http://goo.gl/04j7Nn');
+                        }
+                         *{
+                             color:#5a5a5a;
+                             font-size:11px;
+                             font-weight:bold;
+                             padding-top:14px;
+                             padding-bottom:14px;
+                             padding-left:10px;
+                             padding-right:10px;
+                             text-transform:uppercase;
+                             }
+                                 
+                             .nav a {
+                                 color:#5a5a5a;
+                                 }
