479b36a835accc9008c9b56c611f6718b7c467e0
manuelalanis <alanism@uabc.edu.mx>
2014-07-01 23:59:32 -0700

    variables changes

---
+++ js/bucledo.js
@@ -0,0 +1,8 @@
+cuak=false;
+var alHelicoptero = function(cuak){
+  do{
+      console.log("Esta bluce no durar mucho ya veras !");
+      }while(cuak===true);
+  
+};
+alHelicoptero(cuak);
---
+++ js/variable.js
@@ -0,0 +1,10 @@
+var varible=function(){
+   for(i=0;i<10;i++){
+   console.log("This is i++ syntax");}
+   for(i=0;i<10;i+=3){
+   console.log("This is i+=3 syntax");}
+   for(i=0;i<10;i*=2){
+   console.log("This is i*=2 syntax");}
+
+};
+variable();
