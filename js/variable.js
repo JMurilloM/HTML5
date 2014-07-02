2694039998cc07c3022a4adc6609740006ffc468
manuelalanis <alanism@uabc.edu.mx>
2014-07-02 00:07:19 -0700

    variables changes

--- js/variable.js
+++ js/variable.js
@@ -1,9 +1,9 @@
-var varible=function(){
+var variable=function(){
    for(i=0;i<10;i++){
    console.log("This is i++ syntax");}
    for(i=0;i<10;i+=3){
    console.log("This is i+=3 syntax");}
-   for(i=0;i<10;i*=2){
+   for(i=1;i<10;i*=2){
    console.log("This is i*=2 syntax");}
 
 };
